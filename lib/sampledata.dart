c09d62678d85c57396761fc85117b23c4bb05957
Sahan Perera <sahanyuganthamail@gmail.com>
2024-04-16 20:24:49 +0100

    class seperated to files

--- lib/sampledata.dart
+++ lib/sampledata.dart
@@ -2,17 +2,20 @@ import 'blog_post.dart';
 
 final List<BlogPost> blogPosts = [
   BlogPost(
+    id:0,
     title: 'Post 1',
     description: 'Description of Post 1',
-    imageURL: 'https://cdn-icons-png.flaticon.com/256/2593/2593549.png',
+    imageURL: 'https://cdn-icons-png.flaticon.com/256/2593/2593549.png', 
   ),
   BlogPost(
+    id: 1,
     title: 'Post 2',
     description: 'Description of Post 2',
     imageURL:
     'https://cdn.icon-icons.com/icons2/560/PNG/512/Blog_icon-icons.com_53707.png',
   ),
   BlogPost(
+    id:2,
     title: 'Post 3',
     description: 'Description of Post 3',
   ),
